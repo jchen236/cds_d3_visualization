4f57b02427a3bc608024ded8638db6ae6d252f77
Nikhil Saggi <ns739@cornell.edu>
2018-09-30 12:40:12 -0400

    used geocoding api to get addresses -> coordinates

--- data.py
+++ data.py
@@ -1,15 +1,27 @@
 import pandas as pd
+import urllib.request as urllib2
+import urllib.parse
+import json
+import api
 
-def get_top50_addr():
+def get_top(num):
     df = pd.read_csv('nyc-rolling-sales.csv')
     df['SALE PRICE'] = df['SALE PRICE'].replace(' -  ','0').astype(float)
 
-    top_50_index = df.nlargest(50, 'SALE PRICE').index
+    top_index = df.nlargest(num, 'SALE PRICE').index
 
-    top_50_addr = []
-    for i in top_50_index:
-        top_50_addr.append(df.at[i, 'ADDRESS'])
-    return(top_50_addr)
+    top_addr = []
+    for i in top_index:
+        top_addr.append(str(df.at[i, 'ADDRESS']) + ", " + str(df.at[i, 'ZIP CODE']))
+
+    top_coords = []
+    for loc in top_addr:
+        loc = urllib.parse.quote(loc)
+        url = "https://api.opencagedata.com/geocode/v1/json?q={}&key={}&language=en&pretty=1".format(loc, api.get_key())
+        with urllib2.urlopen(url) as url:
+            data = json.loads(url.read().decode())['results'][0]['bounds']['northeast']
+            top_coords.append((data['lat'], data['lng']))
+    return(top_coords)
 
 if __name__ == '__main__':
-    print(get_top50_addr())
+    print(get_top(10))
